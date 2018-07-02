a2073914dd3711944b030ea8dd21389ce5b0862b
TPJ <514820184@qq.com>
2018-07-02 18:29:24 +0800

    1.0.0测试版

--- src/main/groovy/com.tangpj.calces/ModulesConfigPlugin.groovy
+++ src/main/groovy/com.tangpj.calces/ModulesConfigPlugin.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2018 Tang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.tangpj.calces
 
 import com.android.build.gradle.AppPlugin
@@ -7,7 +22,6 @@ import com.tangpj.calces.extensions.AppExt
 import com.tangpj.calces.extensions.LibraryExt
 import com.tangpj.calces.utils.AppManifestStrategy
 import com.tangpj.calces.utils.LibraryManifestStrategy
-import groovy.util.slurpersupport.GPathResult
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
